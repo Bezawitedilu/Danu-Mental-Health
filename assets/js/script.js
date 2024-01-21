9acccf52c83c4b3179ba8f9df4014dd6101ed347
Bezawit <bezawitedilu@gmail.com>
2024-01-21 10:57:21 +0300

    edited js file to include spinner

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,13 +89,14 @@ const revealElements = document.querySelectorAll("[data-reveal]");
 
 const revealElementOnScroll = function () {
   for (let i = 0, len = revealElements.length; i < len; i++) {
-    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15) {
+    if (
+      revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15 &&
+      !revealElements[i].classList.contains("revealed")
+    ) {
       revealElements[i].classList.add("revealed");
-    } else {
-      revealElements[i].classList.remove("revealed");
     }
   }
-}
+};
 
 window.addEventListener("scroll", revealElementOnScroll);
 window.addEventListener("load", revealElementOnScroll);
